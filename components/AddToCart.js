b3bd10ad093bc65921373c7abe264d950a023e2e
Milkac-Design <milkac88@gmail.com>
2020-10-19 14:29:55 +0200

    Setup Heroku PORT

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 import { addAmountToCartInCookie } from '../utils/cookies.js';
-// import { getCartFromCookies } from '../utils/cookies.js';
+
 
 export default function AddToCart(props) {
   const [quantity, setQuantity] = useState(1);
   const handleChange = (e) => {
     setQuantity(e.target.value);
   };
-  // const setCart = props.setCart[0];
+
 
   function handleAddtoCart(e) {
-    // e.preventDefault();
+
     const id = props.id;
     const quant = quantity;
     console.log(typeof id, id, quant);
     addAmountToCartInCookie(id, parseInt(quant));
     setQuantity('1');
-    // setCart(getCartFromCookies());
+
   }
 
   return (
--- utils/cookies.js
+++ utils/cookies.js
@@ -6,14 +6,12 @@ export function getCartFromCookies() {
 }
 
 export function addAmountToCartInCookie(id, quantity) {
-  // the id and quantity (as integer) are passed to the function
-  // get the old cart from the Cookies
+
   const cart = getCartFromCookies();
   let newCart;
-  // first check if the cart exists (length) and the id is aleady in, filter will return true, then to make the new cart, map over existing cart, and where the id is the same as passed in, increase amount by the quantity
-  // otherwise, cart is empty or does not contain the item yet, push item to new cart
+
   if (cart.length !== 0 && cart.find((item) => item.id === id)) {
-    //console.log(cart);
+
     newCart = cart.map((item) =>
       item.id !== id ? item : { ...item, amount: (item.amount += quantity) },
     );
@@ -23,21 +21,17 @@ export function addAmountToCartInCookie(id, quantity) {
   } else {
     newCart = [{ id: id, amount: quantity }];
   }
-
-  // set the cookie to the new cart and return new cart
   cookie.set('cart', newCart);
   return newCart;
 }
 
 export function updateAmountInCartInCookie(id, quantity) {
-  // the id and quantity (as integer) are passed to the function
-  // get the old cart from the Cookies
+
   const cart = getCartFromCookies();
   let newCart;
-  // first check if the cart exists (length) and the id is aleady in, filter will return true, then to make the new cart, map over existing cart, and where the id is the same as passed in, update amount to the new value
-  // otherwise, cart is empty or does not contain the item yet, push item to new cart
+
   if (cart.length !== 0 && cart.find((item) => item.id === id)) {
-    //console.log(cart);
+
     newCart = cart.map((item) =>
       item.id !== id ? item : { ...item, amount: parseInt(quantity) },
     );
@@ -48,7 +42,7 @@ export function updateAmountInCartInCookie(id, quantity) {
     newCart = [{ id: id, amount: quantity }];
   }
 
-  // set the cookie to the new cart and return new cart
+
   cookie.set('cart', newCart);
   return newCart;
 }
@@ -56,7 +50,7 @@ export function updateAmountInCartInCookie(id, quantity) {
 export function removeItemFromCartInCookie(id) {
   const cart = getCartFromCookies();
   let newCart;
-  // do the stuff to remove the item here
+
 
   if (cart.length !== 0) {
     newCart = cart.filter((item) => item.id !== id);
