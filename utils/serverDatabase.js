cdead2ebc1203f723a20b2ecb30f4224e66bbc58
Milkac-Design <milkac88@gmail.com>
2020-10-19 15:00:07 +0200

    Add SSL configuration for ley

--- utils/serverDatabase.js
+++ utils/serverDatabase.js
@@ -5,7 +5,13 @@ import camelcaseKeys from 'camelcase-keys';
 
 setPostgresDefaultsOnHeroku();
 dotenv.config();
-const sql = postgres();
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 export async function getProducts() {
   const products = await sql`SELECT * from products`;
